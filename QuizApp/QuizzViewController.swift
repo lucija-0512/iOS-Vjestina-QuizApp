37d38969a870a7ddcde05c67a87368a15f713ecf
five <lucija.omrcen@fer.hr>
2021-04-25 00:33:59 +0200

    Add question label

--- QuizApp/QuizzViewController.swift
+++ QuizApp/QuizzViewController.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import PureLayout
 
 class QuizzViewController: UIViewController {
 
     let quiz : Quiz
+    private var questionLabel : UILabel!
 
     init(_quiz: Quiz) {
         self.quiz = _quiz
@@ -22,11 +24,32 @@ class QuizzViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(quiz)
+        buildViews()
+        addConstraints()
         // Do any additional setup after loading the view.
     }
     
-
+    private func buildViews() {
+        view.backgroundColor = .systemBlue
+        
+        questionLabel = UILabel()
+        questionLabel.text = quiz.questions[0].question
+        questionLabel.textColor = .white
+        questionLabel.lineBreakMode = .byWordWrapping
+        questionLabel.numberOfLines = 0
+        questionLabel.font = UIFont.boldSystemFont(ofSize: 20.0)
+        
+        
+        
+        view.addSubview(questionLabel)
+    }
+    
+    private func addConstraints() {
+        questionLabel.autoAlignAxis(toSuperviewMarginAxis: .vertical)
+        questionLabel.autoPinEdge(toSuperviewSafeArea: .top, withInset: 20)
+        questionLabel.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 20)
+        questionLabel.autoPinEdge(toSuperviewSafeArea: .trailing, withInset: 20)
+    }
     /*
     // MARK: - Navigation
 
